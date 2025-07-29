ee36b1e53d7c5b77592a89c483a5337abaefa70f
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-07-29 15:16:29 +0530

    ps

---
+++ pythontasks/150questionsp2.py
@@ -0,0 +1,60 @@
+# #patterns
+# for i in range (1,7):
+#     print("*"*i)
+
+# #2
+# rows=5
+# for i in range(1,rows+1):
+#     print( " "*(rows-i)+"$"*i)
+
+# #3
+# rows=5
+# for i in range(1,rows+1):
+#     spaces="  "*(rows-i)
+#     dollars= " $" * i
+#     print(spaces+dollars)
+
+# #4
+# rows=5
+# for i in range(rows):
+#     print(''*i+"*"*(rows-i))
+
+# #5
+# rows=5
+# for i in range(rows):
+#     print(' '*i+"* "*(rows-i))
+
+# #5
+# num=1
+# rows=4
+# for i in range(1,rows+1):
+#     for j in range(i):
+#         print(num ,end=' ')
+#         num+=1
+#     print()
+
+# #6
+# num=10
+# rows=5
+# for i in range(1,rows+1):
+#     for j in range(i):
+#         print(num,end="  ")
+#         num+=1
+#     print()
+
+# #7
+# rows=5
+# for i in range(rows):
+#     print(' ' * i,end="")
+#     for j in range(rows-i):
+#         if j==0 or j==rows-i-1:
+#             print('*',end=' ')
+#         else:
+#             print(' ',end=' ')
+#     print()
+
+#8
+rows=5
+for i in range (1,6):
+    
+
