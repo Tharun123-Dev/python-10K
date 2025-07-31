87baa1bc531c0d3121395d358b3b2d7638a3a6aa
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-07-31 15:37:19 +0530

    "new

--- pythontasks/inheritance/multiple.py
+++ pythontasks/inheritance/multiple.py
@@ -1,19 +1,20 @@
-# class Parent1:
-#     def P1method(self):
-#         print("im a p1 method")
-# class Parent2:
-#     def P2method(self):
-#         print("am a parent2")
-# class child(Parent1,Parent2):
-#     def Cmethod(self):
-#         print("am c")
-#         super().P1method()
-#         super().P2method()
+class Parent1:
+    def P1method(self):
+        print("im a p1 method")
+class Parent2:
+    def P2method(self):
+        print("am a parent2")
+class child(Parent1,Parent2):
+    def Cmethod(self):
+        print("am c")
+        super().P1method()
+        super().P2method()
 
-# obj1=child()
-# obj1.Cmethod()
-# # obj1.P2method()
-# count=0
-# def is_prime(n):
+obj1=child()
+obj1.Cmethod()
+# obj1.P2method()
+count=0
+def is_prime(n):
+    print("nani")
 
 
