dbf2d2a270cdfd8a65c2e0a4a06918e2608b088d
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-08-01 14:01:26 +0530

    patterns

--- pythontasks/methodoverloading.py
+++ pythontasks/methodoverloading.py
@@ -1,12 +1,12 @@
-# class A:
-#     def add (self,a,b):
-#         return a+b
-#     def add (self,a,b,c):
+class A:
+    def add (self,a,b):
+        return a+b
+    def add (self,a,b,c):
     
-#         return a+b+c
-# obj=A()
-# print(obj.add(1,2,3))
-# print(obj.add(1,2))# it doesn"t take because of latest last one only print
+        return a+b+c
+obj=A()
+print(obj.add(1,2,3))
+#print(obj.add(1,2))# it doesn"t take because of latest last one only print
 
 
 #it is used for printinmg without error
