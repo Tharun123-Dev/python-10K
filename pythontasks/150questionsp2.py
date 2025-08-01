d6e067dc7bd2fe8f52a4b28b15113898bdcb6d62
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-08-01 14:00:36 +0530

    patterns

--- pythontasks/150questionsp2.py
+++ pythontasks/150questionsp2.py
@@ -2,7 +2,8 @@
 # for i in range (1,7):
 #     print("*"*i)
 
-# #2
+# # #2
+# # 
 # rows=5
 # for i in range(1,rows+1):
 #     print( " "*(rows-i)+"$"*i)
@@ -53,8 +54,67 @@
 #             print(' ',end=' ')
 #     print()
 
-#8
-rows=5
-for i in range (1,6):
+# #8
+# rows=5
+# for i in range (1,6):
     
+# #8
+# num=1
+# row=5
+# for i in range(1,row+1):
+#     for j in range(i):
+#         print(num ,end=" ")
+#         num+=1
+#     print()
+# #  #9
+# num=1
+# for i in range (1,5):
+#     if i%2==0:
+#         end=num+i
+#         for j in range(end-1,num-1,-1):
+#             print(j,end=" ")
+#         num=end
+#     else:
+#         for  j in range(i):
+#             print(num,end=" ")
+#             num+=1
+#     print()
+    
+# #10
+# start = 1
+# for i in range(1, 5):        # 4 lines
+#     for j in range(i):       # print i numbers in each line
+#         print(start, end="")
+#         start += 1
+#     if i %2==0:
+#         print(start[::-1])
+#     print()
 
+#11
+# row=5
+# for i in range(1,row):
+#         print(" "*(row-i)+" *"*i)
+# for j in range(row,0,-1):
+#         print(" "*(row-j)+" *"*j)
+        
+
+# #12
+# str="tharunn"
+# k=0
+# for i in range(1,5):
+#     for j in range(i):
+#         if k<len(str):
+#             print(str[k],end=" ")
+#             k+=1
+#         else:
+#             print("*",end=" ")
+#     print()
+
+# #13
+# row=6
+# for i in range (row-1,0,-1):
+#     print (  " "*(row-i)+"* "*i)
+# for j in range (2,row):
+#     print(" "*(row-j)+"* "*j)
+
+#14
