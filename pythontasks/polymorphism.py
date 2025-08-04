f5dbc54828e687bbc001dd7a5d1cb5042b24c2af
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-08-04 14:28:30 +0530

    new

--- pythontasks/polymorphism.py
+++ pythontasks/polymorphism.py
@@ -1,11 +1,13 @@
-# def sum(*args):
-#     if args:
-#         start=type(args[0])()
-#         for i in args:
-#             start+=i
-#         return start
-# print(sum(1,2))
-# print(sum("nani","heh")) 
+
+#1
+def sum(*args):
+    if args:
+        start=type(args[0])()
+        for i in args:
+            start+=i
+        return start
+print(sum(1,2))
+print(sum("nani","heh")) 
 
 #2
 class A:
@@ -27,3 +29,28 @@ obj3=C()
 poly(obj1)
 poly(obj2)
 poly(obj3)
+
+#3
+class Dog:
+    def speak(self):
+        return "woooff"
+class Cat:
+    def speak(self):
+        return "meow"
+class Tiger:
+    def speak(self):
+        return "roar"
+def animal(animals):
+    print(animals.speak())
+
+
+dog=Dog()
+cat=Cat()
+tiger=Tiger()
+
+animal(dog)
+animal(cat)
+animal(tiger)
+    
+    
+    
