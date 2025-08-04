915a2d2f6ab8f7a88d2b8ed9501a38f3cee28c31
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-08-04 14:44:51 +0530

    new

--- pythontasks/polymorphism.py
+++ pythontasks/polymorphism.py
@@ -1,56 +1,73 @@
 
-#1
-def sum(*args):
-    if args:
-        start=type(args[0])()
-        for i in args:
-            start+=i
-        return start
-print(sum(1,2))
-print(sum("nani","heh")) 
+# #1
+# def sum(*args):
+#     if args:
+#         start=type(args[0])()
+#         for i in args:
+#             start+=i
+#         return start
+# print(sum(1,2))
+# print(sum("nani","heh")) 
 
-#2
-class A:
-    def fun(self):
-        print("A")
-class B:
-    def fun(self):
-        print("B")
+# #2
+# class A:
+#     def fun(self):
+#         print("A")
+# class B:
+#     def fun(self):
+#         print("B")
 
-class C:
-    def fun(self):
-        print("C")
-def poly(obj):
-    obj.fun()
+# class C:
+#     def fun(self):
+#         print("C")
+# def poly(obj):
+#     obj.fun()
 
-obj1=A()
-obj2=B()
-obj3=C()
-poly(obj1)
-poly(obj2)
-poly(obj3)
+# obj1=A()
+# obj2=B()
+# obj3=C()
+# poly(obj1)
+# poly(obj2)
+# poly(obj3)
 
-#3
-class Dog:
-    def speak(self):
-        return "woooff"
-class Cat:
-    def speak(self):
-        return "meow"
-class Tiger:
-    def speak(self):
-        return "roar"
-def animal(animals):
-    print(animals.speak())
+# #3
+# class Dog:
+#     def speak(self):
+#         return "woooff"
+# class Cat:
+#     def speak(self):
+#         return "meow"
+# class Tiger:
+#     def speak(self):
+#         return "roar"
+# def animal(animals):
+#     print(animals.speak())
 
 
-dog=Dog()
-cat=Cat()
-tiger=Tiger()
+# dog=Dog()
+# cat=Cat()
+# tiger=Tiger()
 
-animal(dog)
-animal(cat)
-animal(tiger)
+# animal(dog)
+# animal(cat)
+# animal(tiger)
+
+#4
+class SendMsg:
+    def send(self,msg):
+        print(f"sms sent {msg}")
+class SendEmail:
+    def send(self,msg):
+        print(f"email sent: {msg}")
+class Notifier:
+    def send(self,msg):
+        print(f"notifier sent : {msg}")
+def sending(sender,msg):
+    sender.send(msg)
+
+sending(SendMsg(),"hi your otp is 2343")
+sending(SendEmail(),"hi your netflix is about to expired")
+sending(Notifier()," Virus alert!...")
     
     
     
