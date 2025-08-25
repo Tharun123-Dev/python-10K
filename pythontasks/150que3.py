8fe6b7166f9775d31348deff8dc5eb6290d211b9
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-08-25 14:35:48 +0530

    new

--- pythontasks/150que3.py
+++ pythontasks/150que3.py
@@ -64,11 +64,13 @@
     # print(chr(i))
 
 #6
-for i in range(65,91):
-    print(chr(i))
+# for i in range(65,91):
+    # print(chr(i))
 
-for i in range (97,123):
-    print(chr(i))
+# for i in range (97,123):
+    # print(chr(i))
+
+
 
 
 
